09ce04956f87abe6e72133f2b79deb59f60b97c1
Andrej Gorbatniov <andrej.gorbatniov@kachialov.lt>
2025-10-08 20:22:07 +0300

    Simetrinis šifravimas (praktika)

---
+++ Kriptografija/02m-III_a_simetrinis_uzsifravimas.py
@@ -0,0 +1,33 @@
+﻿# PROGRAMOS SIFR_AESR.PY KODAS
+from Crypto.Cipher import AES
+
+def main():
+    # Naudokime pastovų 32 baitų ilgio raktą
+    key = b"ThisIsASecretKey_32bytesLongAbCd"
+    
+    # Inicijuoti AES šifro objektą su EAX režimu
+    cipher = AES.new(key, AES.MODE_EAX)
+    
+    # Teksto užšifravimas
+    plaintext = b"Slapta: Kompiuteris yra tavo ir mano geriausias draugas :-)."
+    ciphertext, tag = cipher.encrypt_and_digest(plaintext)
+    
+    # Atspausdiname raktą, užšifruotą tekstą, tag ir nonce reikšmes
+    print(f"Raktas: {key.decode('utf-8')}")
+    print(f"Užšifruotas tekstas: {ciphertext.hex()}")
+    print(f"Tag: {tag.hex()}")
+    print(f"Nonce: {cipher.nonce.hex()}")  # Atspausdiname nonce reikšmę
+
+    # Teksto iššifravimas
+    cipher_dec = AES.new(key, AES.MODE_EAX, nonce=cipher.nonce)
+    decrypted = cipher_dec.decrypt(ciphertext)
+    
+    # Patikriname, ar iššifruotas tekstas sutampa su pradiniais duomenimis
+    try:
+        cipher_dec.verify(tag)
+        print(f"Pradinis: {decrypted.decode('utf-8')}")
+    except ValueError:
+        print("Iššifruotasis tekstas yra neteisingas!")
+
+if __name__ == '__main__':
+    main()
---
+++ Kriptografija/02m-III_b_simetrinis_issifravimas.py
@@ -0,0 +1,32 @@
+﻿# PROGRAMOS SIFR_AESS.PY KODAS
+from Crypto.Cipher import AES
+from binascii import unhexlify
+
+def decrypt_aes_eax(ciphertext_hex, key, tag_hex, nonce_hex):
+    # Konvertuojame šiuos duomenis iš šešioliktainės į baitų sekas
+    ciphertext = unhexlify(ciphertext_hex)
+    tag = unhexlify(tag_hex)
+    nonce = unhexlify(nonce_hex)
+
+    # Sukuriamas šifravimo objektas su pateiktu raktu ir nonce
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+     
+    # Bandoma iššifruoti tekstą ir patikrinti MAC su pateiktu tag'u
+    try:
+        decrypted_data = cipher.decrypt_and_verify(ciphertext, tag)
+        return decrypted_data.decode('utf-8')
+    except ValueError:
+        return "Užšifruotas tekstas buvo pažeistas. Iššifravimas nutrauktas."
+
+def main():
+    # Pateikti duomenys
+    key = b"ThisIsASecretKey_32bytesLongAbCd"
+    ciphertext_hex = "dbb6fd975fafc6282d8a27dd9841cc26bf67929926e7648fa0b50fdc88bff2641ab5e68b7c447f310d527b6ff2df1f5c4498c4be4ede13"
+    tag_hex = "265df1dfa03b68cd368605e2660c763d"
+    nonce_hex = "78db0191dcadb80bb318579ad3a535d3"
+
+    decrypted_message = decrypt_aes_eax(ciphertext_hex, key, tag_hex, nonce_hex)
+    print(f"Iššifruotas tekstas: {decrypted_message}")
+
+if __name__ == '__main__':
+    main()
